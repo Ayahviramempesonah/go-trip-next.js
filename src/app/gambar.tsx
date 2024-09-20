a940707e893232e2fb30c05934cf297ff4d271c4
bayusatriapurnomo <bayusatriapurnomo@gmail.com>
2024-09-20 17:11:58 +0000

    memecah komponent gambar

---
+++ src/app/gambar.tsx
@@ -0,0 +1,12 @@
+
+export default function Gambar() {
+    return (
+  
+      <img src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/15/1686795553890-aac1f15fc6a913f9e93ead67b75f922f.png" alt="Katherine Johnson" />
+  
+    )
+  
+  
+  
+  }
+    
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,6 +113,7 @@
 // }
 
 
+ import Gambar from "./gambar";
 
 export default function Home() {
 
@@ -202,16 +203,16 @@ export default function Home() {
 }
 
 
-function Gambar() {
-  return (
+// function Gambar() {
+//   return (
 
-    <img src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/15/1686795553890-aac1f15fc6a913f9e93ead67b75f922f.png" alt="Katherine Johnson" />
+//     <img src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/15/1686795553890-aac1f15fc6a913f9e93ead67b75f922f.png" alt="Katherine Johnson" />
 
-  )
+//   )
 
 
 
-}
+// }
 
 
 function Gambar2() {
