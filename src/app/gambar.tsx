c36a1097bb4e92139734855085b5a98fcbb017c4
bayusatriapurnomo <bayusatriapurnomo@gmail.com>
2024-11-03 15:02:51 +0000

    add linik di cabang1

--- src/app/gambar.tsx
+++ src/app/gambar.tsx
@@ -1,12 +1,28 @@
 
 export default function Gambar() {
-    return (
-  
-      <img src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/15/1686795553890-aac1f15fc6a913f9e93ead67b75f922f.png" alt="Katherine Johnson" />
-  
-    )
-  
-  
-  
-  }
-    
+  return (
+    <>
+      <div>
+        <a
+          href="https://www.traveloka.com/en-en/hotel/indonesia?funnel_source=Merchandising.mainAppHomePage.mainAppHomePage-V2-SSR-web-EN-LandingPage&funnel_id=M_1_9ea8e0363311157011bea3fa5414aa65f1694882_0_719354f45bd9a7bd08e5e54478bd37e4ec9f1971&internal_source=true"
+
+          target="_blank"
+         rel="noopener noreferrer"
+>
+        <img
+          src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/15/1686795553890-aac1f15fc6a913f9e93ead67b75f922f.png"
+
+
+          alt="Katherine Johnson" />
+</a>
+        <h3>Indonesia</h3>
+
+      </div>
+    </>
+
+  );
+
+
+
+
+}
